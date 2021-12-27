aaa488885a2e623c2b44e3613ac5d0b1898d6c98
korneychukv <blaker09@mail.ru>
2021-12-27 14:20:40 +0300

    Removed extra files

--- prof/src/test/java/ru/rsatu/NativeProfGateIT.java
+++
@@ -1,9 +0,0 @@
-package ru.rsatu;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeProfGateIT extends ProfGateTest {
-
-    // Execute the same tests but in native mode.
-}
